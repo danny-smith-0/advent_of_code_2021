9c73e258ffdf2343e3cdce9cfe626119f3d55b5a
Danny Smith <danny.smith.0@outlook.com>
2021-12-06 11:14:01 -0500

    Undo day6's changes to day5

--- day5.cpp
+++ day5.cpp
@@ -9,7 +9,6 @@
 
 typedef std::vector<int> ints_t;
 
-// int max_value = 0;
 int count_2s = 0;
 
 ints_t matlab_colon(int first, int last)
@@ -48,116 +47,83 @@ std::vector<ints_t> parse_day5()
 
 int main ()
 {
+    std::vector<ints_t> sets_of_4_ints = parse_day5();
+
+    static constexpr int max_dimension = 1000;
+    std::array<std::array<uint8_t, max_dimension>, max_dimension> cells = {0};
+    // std::array<std::array<uint8_t, max_dimension>, max_dimension> cells2 = {0};
+
+    for (auto set : sets_of_4_ints)
     {
-        // std::vector<ints_t> sets_of_4_ints = parse_day5();
-
-        // static constexpr int max_dimension = 1000;
-        // std::array<std::array<uint8_t, max_dimension>, max_dimension> cells = {0};
-        // // std::array<std::array<uint8_t, max_dimension>, max_dimension> cells2 = {0};
-
-        // for (auto set : sets_of_4_ints)
+        // //Only horizontal & vertical
+        // if (set[0] == set[2])
         // {
-        //     // //Only horizontal & vertical
-        //     // if (set[0] == set[2])
-        //     // {
-        //     //     //add to cells
-        //     //     int min = std::min(set[1], set[3]);
-        //     //     int max = std::max(set[1], set[3]);
-        //     //     for ( int ii = min; ii <= max; ++ii)
-        //     //     {
-        //     //         cells[ii][set[0]]++;
-        //     //         int cell_value = cells[ii][set[0]];
-        //     //         // check_and_count_max(cell_value);
-        //     //         if (cell_value == 2)
-        //     //             count_2s++;
-        //     //     }
-        //     // }
-        //     // else if (set[1] == set[3])
-        //     // {
-        //     //     //add to cells
-        //     //     int min = std::min(set[0], set[2]);
-        //     //     int max = std::max(set[0], set[2]);
-        //     //     for ( int ii = min; ii <= max; ++ii)
-        //     //     {
-        //     //         cells[set[1]][ii]++;
-        //     //         int cell_value = cells[set[1]][ii];
-        //     //         // check_and_count_max(cell_value);
-        //     //         if (cell_value == 2)
-        //     //             count_2s++;
-        //     //     }
-        //     // }
-
-        //     ints_t xs = matlab_colon(set[0], set[2]);
-        //     ints_t ys = matlab_colon(set[1], set[3]);
-        //     if (xs.size() == 1 && ys.size() > 1)
-        //         xs = ints_t(ys.size(), xs[0]);
-        //     if (ys.size() == 1 && xs.size() > 1)
-        //         ys = ints_t(xs.size(), ys[0]);
-        //     if (ys.size() != xs.size())
-        //         std::cout << "Size error!\n";
-        //     int kk = set[0];
-        //     for (int ii = 0; ii < xs.size() ; ++ii)
+        //     //add to cells
+        //     int min = std::min(set[1], set[3]);
+        //     int max = std::max(set[1], set[3]);
+        //     for ( int ii = min; ii <= max; ++ii)
         //     {
-        //         int cell_value = ++cells[ys[ii]][xs[ii]];
+        //         cells[ii][set[0]]++;
+        //         int cell_value = cells[ii][set[0]];
+        //         // check_and_count_max(cell_value);
         //         if (cell_value == 2)
-        //                 count_2s++;
+        //             count_2s++;
         //     }
         // }
-
-        // std::cout << "\n\n" << /*"max_value: " << max_value << ", " <<*/ count_2s << " times" << "\n\n";
-
-        // // Plot
-        // std::ofstream out("out.txt");
-        // for (auto rows : cells)
+        // else if (set[1] == set[3])
         // {
-        //     for (auto cell : rows)
+        //     //add to cells
+        //     int min = std::min(set[0], set[2]);
+        //     int max = std::max(set[0], set[2]);
+        //     for ( int ii = min; ii <= max; ++ii)
         //     {
-        //         if (cell == 0)
-        //         {
-        //             // std::cout << ".";
-        //             out << "0,";
-        //         }
-        //         else
-        //         {
-        //             // std::cout << static_cast<int>(cell);// << " ";
-        //             out << static_cast<int>(cell) << ",";
-        //         }
+        //         cells[set[1]][ii]++;
+        //         int cell_value = cells[set[1]][ii];
+        //         // check_and_count_max(cell_value);
+        //         if (cell_value == 2)
+        //             count_2s++;
         //     }
-        //     // std::cout << "\n";
-        //     out << "\n";
         // }
+
+        ints_t xs = matlab_colon(set[0], set[2]);
+        ints_t ys = matlab_colon(set[1], set[3]);
+        if (xs.size() == 1 && ys.size() > 1)
+            xs = ints_t(ys.size(), xs[0]);
+        if (ys.size() == 1 && xs.size() > 1)
+            ys = ints_t(xs.size(), ys[0]);
+        if (ys.size() != xs.size())
+            std::cout << "Size error!\n";
+        int kk = set[0];
+        for (int ii = 0; ii < xs.size() ; ++ii)
+        {
+            int cell_value = ++cells[ys[ii]][xs[ii]];
+            if (cell_value == 2)
+                    count_2s++;
+        }
     }
 
-    // ints_t fishys = {3,4,3,1,2};
-    ints_t fishys = {5,1,2,1,5,3,1,1,1,1,1,2,5,4,1,1,1,1,2,1,2,1,1,1,1,1,2,1,5,1,1,1,3,1,1,1,3,1,1,3,1,1,4,3,1,1,4,1,1,1,1,2,1,1,1,5,1,1,5,1,1,1,4,4,2,5,1,1,5,1,1,2,2,1,2,1,1,5,3,1,2,1,1,3,1,4,3,3,1,1,3,1,5,1,1,3,1,1,4,4,1,1,1,5,1,1,1,4,4,1,3,1,4,1,1,4,5,1,1,1,4,3,1,4,1,1,4,4,3,5,1,2,2,1,2,2,1,1,1,2,1,1,1,4,1,1,3,1,1,2,1,4,1,1,1,1,1,1,1,1,2,2,1,1,5,5,1,1,1,5,1,1,1,1,5,1,3,2,1,1,5,2,3,1,2,2,2,5,1,1,3,1,1,1,5,1,4,1,1,1,3,2,1,3,3,1,3,1,1,1,1,1,1,1,2,3,1,5,1,4,1,3,5,1,1,1,2,2,1,1,1,1,5,4,1,1,3,1,2,4,2,1,1,3,5,1,1,1,3,1,1,1,5,1,1,1,1,1,3,1,1,1,4,1,1,1,1,2,2,1,1,1,1,5,3,1,2,3,4,1,1,5,1,2,4,2,1,1,1,2,1,1,1,1,1,1,1,4,1,5};
+    std::cout << "\n\n" << /*"max_value: " << max_value << ", " <<*/ count_2s << " times" << "\n\n";
 
-    static constexpr int days = 256;
-
-    for ( int ii = 0; ii < days; ++ii)
+    // Plot
+    std::ofstream out("out.txt");
+    for (auto rows : cells)
     {
-        int count_add = 0;
-        static constexpr int max = 6;
-        for ( auto& fish : fishys)
+        for (auto cell : rows)
         {
-            --fish;
-            if (fish < 0)
+            if (cell == 0)
             {
-                ++count_add;
-                fish = max;
+                // std::cout << ".";
+                out << "0,";
+            }
+            else
+            {
+                // std::cout << static_cast<int>(cell);// << " ";
+                out << static_cast<int>(cell) << ",";
             }
         }
-        ints_t baby_fish (count_add, 8);
-        fishys.insert(fishys.end(), baby_fish.begin(), baby_fish.end());
-
-        // for ( auto fish : fishys)
-        //     std::cout << fish << ",";
-        // std::cout << std::endl;
-        std::cout << "\nafter " << ii << " days, num fish: " << fishys.size();
+        // std::cout << "\n";
+        out << "\n";
     }
-
-    // for ( auto fish : fishys)
-        // std::cout << fish << ",";
-    std::cout << "\nnum fish: " << fishys.size();
 
     return 0;
 }
