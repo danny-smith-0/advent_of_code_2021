15ddff8bb975fea429ad346f1b748abb1e8a7bcd
Danny Smith <danny.smith.0@outlook.com>
2021-12-06 17:27:19 -0500

    Added results printing function

--- include/other_aoc_utils.h
+++ include/other_aoc_utils.h
@@ -16,4 +16,17 @@ ints_t matlab_colon(int first, int last)
     return vector;
 }
 
+template <typename T>
+void results(T test_result, T expected_test_result, T real_result)
+{
+    std::cout << "Test result is " << test_result;
+    if (test_result == expected_test_result)
+    {
+        std::cout << ". Passed!\n";
+        std::cout << "Real result is " << real_result << std::endl;
+    }
+    else
+        std::cout << ". Failed. Looking for " << expected_test_result << "\n";
+}
+
 #endif // OTHER_AOC_UTILS_H
